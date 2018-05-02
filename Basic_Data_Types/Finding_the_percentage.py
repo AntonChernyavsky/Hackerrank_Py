6c8d6d87706824cb4d0861599ddbccba525df249
Anton Charniauski <tony231293@gmail.com>
2018-05-03 00:17:45 +0300

    add new task

---
+++ Basic_Data_Types/Finding_the_percentage.py
@@ -0,0 +1,14 @@
+'''
+    Task: https://www.hackerrank.com/challenges/finding-the-percentage/problem
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = sum(scores)/len(scores)
+    query_name = input()
+
+    print ('%.2f'%student_marks[query_name])
