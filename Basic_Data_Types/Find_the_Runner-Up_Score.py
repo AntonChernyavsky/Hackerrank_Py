9d640cd39140d2ae6a456c20275229f53d633354
Anton Charniauski <tony231293@gmail.com>
2018-05-03 00:08:30 +0300

    add new task

---
+++ Basic_Data_Types/Find_the_Runner-Up_Score.py
@@ -0,0 +1,13 @@
+'''
+    Task: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+arr=list(arr)
+arr.sort()
+a=arr.index(max(arr))
+
+print(arr[a-1])
